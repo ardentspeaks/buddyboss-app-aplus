42ee3fa66d5e5c85b33ec8cbf889a1ca4aefa812
ardentspeaks <105333312+ardentspeaks@users.noreply.github.com>
2022-06-06 20:58:26 -0700

    Initial

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+import * as React from "react";
+import { LoginScreen, View, Text, StyleSheet, Button, Modal, TouchableOpacity, Alert, TextInput } from "react-native";
+
+const aPlusLogin = (props) => (
+	<View style={{flex: 1, justifyContent: "center", alignSelf: "center"}}>
+   		<Text>This is a custom login screen</Text>
+   		<Button title="Login" onPress={() => props.navigation.navigate("Auth")}/>
+   		<Text>Privacy</Text>
+ 	</View>
+);
+
+aPlusLogin.navigationOptions = {header: null};
+
 export const applyCustomCode = externalCodeSetup => {
-	// call custom code api here
+	externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", aPlusLogin);
+
+	externalCodeSetup.settingsScreenApi.setSettingsListFilter((oldTabs, props) => {
+		console.log( oldTabs )
+	});
 };
+
