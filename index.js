d91a0bb2ac159ac92f4cdcf54987441d3a1d4d97
ardentspeaks <105333312+ardentspeaks@users.noreply.github.com>
2022-06-15 09:37:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import * as React from "react";
-import { LoginScreen, Image, View, Text, StyleSheet, Button, Modal, TouchableOpacity, Alert, TextInput } from "react-native";
-
-const aPlusLogin = (props) => (
-	<LoginScreen hideForgotPassword=true/>
-);
-
-export const applyCustomCode = externalCodeSetup => {
-	externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", aPlusLogin);
+export const applyCustomCode = (externalCodeSetup: any) => {
+	// call custom code api here
 };
