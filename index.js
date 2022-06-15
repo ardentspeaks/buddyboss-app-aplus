f88ad00a4d2f211a18578d61ae767704ab6e01db
ardentspeaks <105333312+ardentspeaks@users.noreply.github.com>
2022-06-14 20:04:02 -0700

    Testing changes

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 import * as React from "react";
-import { LoginScreen, View, Text, StyleSheet, Button, Modal, TouchableOpacity, Alert, TextInput } from "react-native";
+import { LoginScreen, Image, View, Text, StyleSheet, Button, Modal, TouchableOpacity, Alert, TextInput } from "react-native";
 
 const aPlusLogin = (props) => (
-	<View style={{flex: 1, justifyContent: "center", alignSelf: "center"}}>
-   		<Text>This is a custom login screen</Text>
-   		<Button title="Login" onPress={() => props.navigation.navigate("Auth")}/>
-   		<Text>Privacy</Text>
- 	</View>
+	<LoginScreen hideForgotPassword=true/>
 );
-
-aPlusLogin.navigationOptions = {header: null};
 
 export const applyCustomCode = externalCodeSetup => {
 	externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", aPlusLogin);
-
-	externalCodeSetup.settingsScreenApi.setSettingsListFilter((oldTabs, props) => {
-		console.log( oldTabs )
-	});
 };
-
